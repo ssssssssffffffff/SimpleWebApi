c87412372792c66986126b267bd9aade0ee812a8
ssssssssffffffff <sntyshrd@gmail.com>
2019-04-30 18:10:17 +0900

    addcomment2

--- SimpleWebApi/Startup.cs
+++ SimpleWebApi/Startup.cs
@@ -14,7 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using SimpleWebApi.Data;
 using NSwag.AspNetCore;
 
-// .net core+SQL Database
+// .net core+SQL Database2
 namespace SimpleWebApi
 {
     public class Startup
