9fc96b3ac21d7bde5b5b7163dc64426ea87b1421
ssssssssffffffff <sntyshrd@gmail.com>
2019-04-30 18:13:45 +0900

    git commit3333

--- SimpleWebApi/Startup.cs
+++ SimpleWebApi/Startup.cs
@@ -14,7 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using SimpleWebApi.Data;
 using NSwag.AspNetCore;
 
-// .net core+SQL Database2
+// .net core+SQL Database3
 namespace SimpleWebApi
 {
     public class Startup
